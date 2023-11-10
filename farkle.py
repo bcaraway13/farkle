297687726f7277d0945851c1fbb584ac5e7bcdad
Blake <bcaraway13@gmail.com>
2023-11-10 13:19:14 -0600

    working program -- needs testing

--- farkle.py
+++ farkle.py
@@ -1,4 +1,6 @@
+from collections import Counter
 import random
+import os
 
 WINNING_SCORE = 10000
 number_of_players = 0
@@ -11,10 +13,11 @@ class Player:
         self.farkles_in_a_row = 0
 
 class Score:
-    def __init__(self, name, points, number_of_dice):
+    def __init__(self, name, points, dice_banked):
         self.name = name
         self.points = points
-        self.number_of_dice = number_of_dice
+        self.dice_banked = dice_banked
+           
 
 class Die:
     def __init__(self):
@@ -24,11 +27,6 @@ class Die:
     def roll(self):
         self.value = random.randint(1, 6)
 
-    def select(self):
-        self.selected = True
-
-    def deselect(self):
-        self.selected = False
 
 class Roll:
     def __init__(self, number_of_dice):
@@ -37,53 +35,50 @@ class Roll:
         else:
             raise ValueError("Number of dice must be between 1 and 6")
         
+        
     def dice(self):
         return [die.value for die in self.dice]
-        
+    
     def roll_all(self):
         for die in self.dice:
-            if not die.selected:
-                die.roll()
+            die.roll()
+        
 
-    def select_die(self, index):
-        self.dice[index].select()
-
-    def deselect_die(self, index):
-        self.dice[index].deselect()
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 
-
-# def roll_dice(number_of_dice):
-#     roll = []
-#     for _ in range(number_of_dice):
-#         die = Die.roll()
-#         roll.append(die.value)
-
-#     return roll
-
-
-def calculate_remaining_dice(dice):
+def calculate_remaining_dice(dice, scores):
     remaining_dice = len(dice)
     ...
     return remaining_dice
 
 
 def calculate_score(dice):
-    score = 0
+    roll_score = 0
+    dice_banked = 0
     possible_scores = check_for_scores(dice)
-    print(possible_scores)
-    # if len(possible_scores) > 0:
-    #     for i in possible_scores:
-
-            
+    possible_scores.sort(key=lambda x: x.points, reverse=True)
     
-
-
+    if len(possible_scores) == 0:
+        print("No scoring dice, Farkle!")
+        return roll_score, dice_banked
     
-    return score
+    print("Scores: ")
+    for score in possible_scores:
+        print(score.name, " - ", score.points)
+        roll_score += score.points
+        dice_banked += score.dice_banked
+    
+    return roll_score, dice_banked
 
 
 def check_for_scores(Roll):
+    # create score objects
+    
+    three_of_a_kind = Score("Three-of-a-Kind", 100, 3)
+    four_of_a_kind = Score("Four-of-a-Kind", 1000, 4)
+
     dice = [die.value for die in Roll.dice]
     possible_scores = []
     pairs = 0
@@ -92,55 +87,55 @@ def check_for_scores(Roll):
     
     for i in range(1,7):
         # Check for pairs
-        if dice.count(i) == 2:
+        if dice.count(i) == 2 and i != 1:
             pairs += 1
 
         # Check for 3 of a kind
         if dice.count(i) == 3:
             if i == 1:
-                possible_scores.append({"Three Ones": 1000})
+                possible_scores.append(Score("Three Ones", 1000, 3))
             else:
-                possible_scores.append({"Three-of-a-Kind": 100 * i})
+                possible_scores.append(Score("Three-of-a-Kind", 100 * i, 3))
             triplets += 1
 
         # Check for 4 of a kind
         elif dice.count(i) == 4:
-            possible_scores.append({"Four-of-a-Kind": 1000})
+            possible_scores.append(Score("Four-of-a-Kind", 1000, 4))
             four_of_a_kinds += 1
 
         # Check for 5 of a kind
         elif dice.count(i) == 5:
-            possible_scores.append({"Five-of-a-Kind": 2000})
+            possible_scores.append(Score("Five-of-a-Kind", 2000, 5))
 
         # Check for 6 of a kind
         elif dice.count(i) == 6:
-            possible_scores.append({"Six-of-a-Kind": 3000})
+            possible_scores.append(Score("Six-of-a-Kind", 3000, 6))
 
     # Check for 1-6 straight
     if sorted(dice) == [1, 2, 3, 4, 5, 6]:
-        possible_scores.append({"Straight": 1500})
+        possible_scores.append(Score("Straight", 1500, 6))
 
     # Check for 3 pairs
     if pairs == 3:
-        possible_scores.append({"Three Pairs": 1500})
+        possible_scores.append(Score("Three Pairs", 1500, 6))
 
     # Check for 4 of a kind plus a pair
     if four_of_a_kinds == 1 and pairs == 1:
-        possible_scores.append({"Four-of-a-Kind + Pair": 1500})
+        possible_scores.append(Score("Four-of-a-Kind + Pair", 1500, 6))
 
     # Check for 2 triplets
     if triplets == 2:
-        possible_scores.append({"Two Triplets": 2500})
+        possible_scores.append(Score("Two Triplets", 2500, 6))
 
     # Check for ones scores
-    if 1 in dice:
-        if dice.count(1) != 3:
-            possible_scores.append({"Ones": dice.count(1) * 100})
+    if 1 in dice and triplets != 2 and pairs != 3:
+        if dice.count(1) < 3:
+            possible_scores.append(Score("Ones", 100 * dice.count(1), dice.count(1)))
 
     # Check for fives scores
-    if 5 in dice:
+    if 5 in dice and triplets != 2 and pairs != 3:
         if dice.count(5) < 3:
-            possible_scores.append({"Fives": dice.count(5) * 50})
+            possible_scores.append(Score("Fives", 50 * dice.count(5), dice.count(5)))
             
     return possible_scores
 
@@ -165,18 +160,38 @@ def get_player_names(player_number):
 def player_turn(player):
     turn_score = 0
     remaining_dice = 6
+    roll_number = 1
 
+    print("\n----------------------------------------\n")
     while True:
         roll = Roll(remaining_dice)
         roll.roll_all()
         dice = [die.value for die in roll.dice]
-        print(f"{player.name} rolled:", dice)
-        
-        score = calculate_score(roll)
-        if score == 0:
-            return turn_score
+        print(f"Roll {roll_number}: {player.name} rolled:", dice)        
+        score, dice_banked = calculate_score(roll)
+        if score == 0 and dice_banked == 0:
+            return 0
         turn_score += score
-        remaining_dice = calculate_remaining_dice(roll)
+        remaining_dice -= dice_banked
+
+        if remaining_dice == 0 and turn_score != 0:
+            remaining_dice = 6
+
+        print(f"Turn score: {turn_score} | Remaining dice: {remaining_dice}")
+        roll_again = ""
+        while True:
+            roll_again = input("Roll again? (y/n): ")
+            if roll_again.lower() != "y" and roll_again.lower() != "n":
+                print("Invalid input. Please enter 'y' or 'n'.")
+            else:
+                break
+        if roll_again.lower() == "n":
+                print()
+                break
+        elif roll_again.lower() == "y":
+            roll_number += 1
+            print()
+            continue
         
     return turn_score
 
@@ -186,11 +201,11 @@ def play_game():
     player_scores = [player.score for player in players]
     current_player = players[0]
     player_names = [player.name for player in players]
-    print("Players: ", player_names)
+    res = str(player_names)[1:-1]
+    print("Players: " + res)
 
     while max(player_scores) < WINNING_SCORE:
         turn_score = player_turn(current_player)
-        current_player.score += turn_score
 
         if turn_score > 0:
             current_player.farkles_in_a_row = 0
@@ -200,6 +215,14 @@ def play_game():
                 print(f"{current_player.name} is out!")
                 players.remove(current_player)
 
+        if current_player.score == 0:
+            if 0 < turn_score < 500:
+                print("First score must be at least 500.")
+                turn_score = 0
+            elif turn_score >= 500:
+                print("You scored at least 500 points, you are on the board!")
+
+        current_player.score += turn_score
 
         if current_player.score >= WINNING_SCORE:
             print(f"{current_player.name} wins!")
@@ -208,11 +231,15 @@ def play_game():
         if len(players) == 1:
             print(f"{players[0].name} wins!")
             break
-
+        
         current_player = players[(players.index(current_player) + 1) % len(players)]
         if current_player == players[0]:
-            for player in players:
+            print("\n----------------------------------------")
+            print("| Next round |")
+            sorted_players = sorted(players, key=lambda x: x.score, reverse=True)
+            for player in sorted_players:
                 print(f"{player.name}: {player.score}")
+            print("----------------------------------------\n")
             
         
 
